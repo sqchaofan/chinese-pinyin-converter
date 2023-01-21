9db41e5214ec16dcfb6b83bb01a68d87fd0dc33c
sqChaofan <darkkwkm08@outlook.com>
2023-01-21 16:28:05 +0900

    change base path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/chinese-pinyin-converter/",
   plugins: [vue()],
   resolve: {
     alias: {
